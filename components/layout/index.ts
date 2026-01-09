3eda0a5b39ed28ee2b3c6870297cd61189e4235f
Prakriti Bista <53210833+itsmepraks@users.noreply.github.com>
2026-01-08 20:52:43 -0500

    feat(layout): create index file for layout component exports

---
+++ components/layout/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Layout Component Exports
+ * 
+ * Centralized exports for all layout components
+ * 
+ * @example
+ * import { Header, Footer } from '@/components/layout';
+ */
+
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
